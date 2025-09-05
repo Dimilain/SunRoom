0d4de045e069578f4bfd1751a882f7045428f941
Dimilain <64850615+Dimilain@users.noreply.github.com>
2025-09-05 03:43:24 +0300

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,12 @@
+js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
